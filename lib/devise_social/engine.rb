914a4271e9bfdb515f18e5ded7cdd85463d79f24
Umair Siddique <umairsiddique@gmail.com>
2012-12-12 19:08:23 +0500

    Updated engine.rb

--- lib/devise_social/engine.rb
+++ lib/devise_social/engine.rb
@@ -4,6 +4,8 @@ module DeviseSocial
       g.integration_tool :rspec
       g.test_framework :rspec
       g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.assets false
+      g.helper false
     end
   end
 end
