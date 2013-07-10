951bc85b9c9dc180e359592a86caf141e45267ad
Zaid Akram <zaid.akram@kwanso.com>
2013-07-10 14:09:33 +0500

    Strong Parameters #Respect

--- app/models/social_authentication.rb
+++ app/models/social_authentication.rb
@@ -6,7 +6,8 @@ class SocialAuthentication < ActiveRecord::Base
 
   class << self
     def from_auth_hash(auth_hash)
-      social_authentication = find_or_initialize_by(provider: auth_hash[:provider], uid: auth_hash[:uid])
+      params = ActionController::Parameters.new(auth_hash).permit(:provider, :uid)
+      social_authentication = find_or_initialize_by(params)
       social_authentication.store_auth_hash(auth_hash)
       social_authentication.save
       social_authentication
@@ -14,5 +15,7 @@ class SocialAuthentication < ActiveRecord::Base
   end
 
   def store_auth_hash(auth_hash)
+    self.token = auth_hash[:credentials][:token]
+    self.expires_at = Time.at(auth_hash[:credentials][:expires_at])
   end
 end
